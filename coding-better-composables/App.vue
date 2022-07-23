7a952d889f47361a686e66ba92bb7cbfc45eac5c
CRBroughton <crbroughton@posteo.uk>
2022-07-23 10:05:00 +0100

    feat(composables): add App.vue to demo composable

---
+++ coding-better-composables/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useMouse } from './mouse'
+
+const { x, y } = useMouse()
+</script>
+
+<template>
+  Mouse position is at: {{ x }}, {{ y }}
+</template>
+
+<style scoped>
+
+</style>
