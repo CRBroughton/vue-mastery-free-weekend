a9c7fa5ef903d6b1b48333c7de18487659581ec8
CRBroughton <crbroughton@posteo.uk>
2022-07-23 10:11:39 +0100

    feat(composables): add undo composable example

--- coding-better-composables/App.vue
+++ coding-better-composables/App.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
-import { useTitle } from '@vueuse/core'
+import { ref } from 'vue'
+import { useRefHistory, useTitle } from '@vueuse/core'
 import { useMouse } from './mouse'
 
 const { x, y } = useMouse()
 const title = useTitle('Composables Demo', { titleTemplate: '%s | My Demo' })
+
+const count = ref(0)
+const { undo } = useRefHistory(count)
 </script>
 
 <template>
   Mouse position is at: {{ x }}, {{ y }}
+
+  <div>
+    Count is {{ count }}
+    <button @click="count++">
+      Increase Count
+    </button>
+    <button @click="undo">
+      Undo Count
+    </button>
+  </div>
 </template>
 
 <style scoped>
